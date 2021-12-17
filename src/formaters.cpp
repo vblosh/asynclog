89ae073a2b7aa05f6164cfc1dbabe9048586c1b5
Vyacheslav Bloshchanevich <slava.pnp@hotmail.com>
2021-12-17 17:50:19 +0100

    secure localtime added

--- src/formaters.cpp
+++ src/formaters.cpp
@@ -10,7 +10,15 @@ std::array<std::string, 7> LogFormatter::labels{ "TRACE", "DEBUG", "INFO ", "WAR
 
 void LogFormatter::Format(std::ostream& buf, const Logdata& logdata)
 {
-    auto tm = *std::localtime(&logdata.timestamp);
+    std::tm tm;
+#if defined(__unix__)
+    localtime_r(&logdata.timestamp, &tm);
+#elif defined(_MSC_VER)
+    localtime_s(&tm, &logdata.timestamp);
+#else
+    tm = *std::localtime(&timer);
+#endif
+
     buf << std::put_time(&tm, timeformat.c_str()) << ' ';
     buf << labels[(unsigned char)logdata.severity] << " : ";
     if (!logdata.area.empty()) {
