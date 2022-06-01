72f9b679412b3d4f8c1b42e7a56ae26e85b0cd86
Vyacheslav Bloshchanevich <slava.pnp@hotmail.com>
2022-06-01 22:20:54 +0200

    spin_pause() added

--- src/mpscqueue.h
+++ src/mpscqueue.h
@@ -6,6 +6,36 @@
 
 using namespace std;
 
+#if defined(_MSC_VER)
+
+	/* prefer using intrinsics directly instead of winnt.h macro */
+	/* http://software.intel.com/en-us/forums/topic/296168 */
+	#include <intrin.h>
+	#if defined(_M_AMD64) || defined(_M_IX86)
+	#pragma intrinsic(_mm_pause)
+	#define spin_pause()  _mm_pause()
+	#elif defined(_M_IA64)
+	#pragma intrinsic(__yield)
+	#define spin_pause()  __yield()
+	#else
+	#define spin_pause()  YieldProcessor()
+	#endif
+#elif defined(__x86_64__) || defined(__i386__)
+
+/* http://software.intel.com/sites/products/documentation/studio/composer/en-us/2011Update/compiler_c/intref_cls/common/intref_sse2_pause.htm
+ * http://software.intel.com/sites/default/files/m/2/c/d/3/9/25602-17689_w_spinlock.pdf
+ * http://software.intel.com/en-us/forums/topic/309231
+ * http://siyobik.info.gf/main/reference/instruction/PAUSE
+ * http://stackoverflow.com/questions/7086220/what-does-rep-nop-mean-in-x86-assembly
+ * http://stackoverflow.com/questions/7371869/minimum-time-a-thread-can-pause-in-linux
+ */
+ /* http://gcc.gnu.org/onlinedocs/gcc/X86-Built_002din-Functions.html
+  * GCC provides __builtin_ia32_pause() */
+ #define spin_pause()  __builtin_ia32_pause()
+
+#endif
+
+
 namespace asynclog
 {
 
@@ -38,7 +68,7 @@ struct spinlock {
 			while (lock_.load(std::memory_order_relaxed)) {
 				// Issue X86 PAUSE or ARM YIELD instruction to reduce contention between
 				// hyper-threads
-				__builtin_ia32_pause();
+				spin_pause();
 			}
 		}
 	}
